cdff33b865daced0bf7b25d0f474c3db55e7436d
Dh-Van <dhvan17shah@gmail.com>
2024-07-24 19:58:59 -0400

    Clean Commit for Restart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,5 @@
 import React from "react";
-import Sidebar from "./Sidebar";
-import Workspace from "./Workspace";
 
 export default function App() {
-	const sampleData = ["test", "task 1", "task 2", "test 2"];
-	const [tasks, setTasks] = React.useState(sampleData);
-	const [clickedElement, setClickedElement] = React.useState(null);
-
-	function handleSubmit(val) {
-		setTasks((prevState) => [...prevState, val]);
-	}
-
-	function editTask(val, idx) {
-		console.log(idx, tasks.length);
-		if (idx >= tasks.length) {
-			setTasks((prevState) => [...prevState, val]);
-		}
-		setTasks((prevState) => prevState.with(idx, val));
-	}
-
-	function addEmptyTask() {
-		setTasks((prevState) => [...prevState, ""]);
-	}
-
-	// console.log(tasks);
-
-	return (
-		<div className="app" onClick={(e) => setClickedElement(e.target)}>
-			<Sidebar />
-			<Workspace
-				tasks={tasks}
-				enter={handleSubmit}
-				editTask={editTask}
-				addEmptyTask={addEmptyTask}
-				clickedElement={clickedElement}
-			/>
-		</div>
-	);
+	return <h1>Hello World!</h1>;
 }
--- src/components/Sidebar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Sidebar(){
-    return (
-        <div className="sidebar">
-            <h2>Dhvan</h2>
-        </div>
-    )
-}
